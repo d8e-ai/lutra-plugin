23b78eab219d6b8d57abd3ed21098a8abe67ea40
Jaran Charumilind <src@jaran.mokchar.com>
2023-12-15 14:15:49 -0800

    Return dataclass representing meetings

--- zoom/plugin.py
+++ zoom/plugin.py
@@ -1,26 +1,63 @@
-import httpx
+from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
+from typing import Optional
+from zoneinfo import ZoneInfo
+
+import httpx
 
 
 from lutraai.augmented_request_client import AugmentedTransport
 
 
-def zoom_list_meetings() -> dict[str, Any]:
+@dataclass
+class ZoomMeeting:
+    uuid: str
+    id: str
+    created_at: datetime
+    host_id: str
+    start_time: datetime
+    join_url: str
+    topic: str
+    agenda: Optional[str]
+    duration_minutes: int
+    meeting_type: int
+
+
+def _to_zoom_meeting(meeting: dict[str, Any]) -> ZoomMeeting:
+    return ZoomMeeting(
+        uuid=meeting["uuid"],
+        id=meeting["id"],
+        created_at=datetime.fromisoformat(meeting["created_at"]),
+        host_id=meeting["host_id"],
+        start_time=(
+            datetime.fromisoformat(meeting["start_time"]).astimezone(
+                tz=ZoneInfo(meeting["timezone"])
+            )
+        ),
+        join_url=meeting["join_url"],
+        topic=meeting["topic"],
+        agenda=meeting.get("agenda"),
+        duration_minutes=meeting["duration"],
+        meeting_type=meeting["type"],
+    )
+
+
+def zoom_list_meetings() -> list[ZoomMeeting]:
     """List all Zoom meetings."""
     with httpx.Client(
         transport=AugmentedTransport(actions_v0.authenticated_request_zoom)
     ) as client:
         # TODO: Add pagination support.
-        return (
+        data = (
             client.get("https://api.zoom.us/v2/users/me/meetings")
             .raise_for_status()
             .json()
         )
+    return [_to_zoom_meeting(meeting) for meeting in data["meetings"]]
 
 
-def zoom_create_meeting(topic: str, start_time: datetime) -> str:
-    """Create a Zoom meeting and return the join URL."""
+def zoom_create_meeting(topic: str, start_time: datetime) -> ZoomMeeting:
+    """Create a Zoom meeting and return it."""
     with httpx.Client(
         transport=AugmentedTransport(actions_v0.authenticated_request_zoom)
     ) as client:
@@ -32,6 +69,4 @@ def zoom_create_meeting(topic: str, start_time: datetime) -> str:
             .raise_for_status()
             .json()
         )
-        if "join_url" not in data:
-            raise ValueError("The 'join_url' is not present in the response data.")
-        return data["join_url"]
+    return _to_zoom_meeting(data)
