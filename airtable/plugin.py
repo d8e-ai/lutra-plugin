db9f5a1b5788733cdf5186b44a5ad686dc0ddb68
Jaran Charumilind <jjc@d8e.ai>
2024-01-03 16:55:12 -0800

    Add Airtable plugin (#22)

---
+++ airtable/plugin.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any
+
+import httpx
+
+from lutraai.augmented_request_client import AugmentedTransport
+
+
+@dataclass
+class AirtableRecord:
+    id: str
+    created_time: datetime
+    fields: dict[str, Any]
+
+
+def airtable_record_list(baseId: str, tableIdOrName: str) -> list[AirtableRecord]:
+    """
+    Return results of an Airtable `list records` API call.
+    """
+    with httpx.Client(
+        transport=AugmentedTransport(actions_v0.authenticated_request_airtable)
+    ) as client:
+        data = (
+            client.get(f"https://api.airtable.com/v0/{baseId}/{tableIdOrName}")
+            .raise_for_status()
+            .json()
+        )
+    return [
+        AirtableRecord(
+            id=record["id"],
+            created_time=datetime.fromisoformat(record["createdTime"]),
+            fields=record["fields"],
+        )
+        for record in data["records"]
+    ]
+
+
+def airtable_record_update_patch(
+    baseId: str, tableIdOrName: str, recordId: str, fields: dict[str, Any]
+) -> None:
+    """
+    Update a record using the Airtable `update record` API call with a PATCH.
+    """
+    with httpx.Client(
+        transport=AugmentedTransport(actions_v0.authenticated_request_airtable)
+    ) as client:
+        client.patch(
+            f"https://api.airtable.com/v0/{baseId}/{tableIdOrName}/{recordId}",
+            json={"fields": fields},
+        ).raise_for_status()
