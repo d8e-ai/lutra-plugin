4df72121621196fb1ec192fb5cc993a5359e927a
Jaran Charumilind <src@jaran.mokchar.com>
2024-02-05 11:15:51 -0800

    Refine types and docstring

--- webhook/plugin.py
+++ webhook/plugin.py
@@ -3,6 +3,12 @@ from typing import Any
 import httpx
 
 
-async def webhook_request(url: str, body: dict[str, Any]) -> Any:
+async def webhook_request(url: str, body: Any) -> Any:
+    """
+    Makes a POST request to the given URL with the given body and returns the response.
+
+    Both the request and response bodies are expected to be JSON and are converted to
+    Python values using the json module.
+    """
     async with httpx.AsyncClient() as client:
         return await client.post(url, json=body).raise_for_status().json()
